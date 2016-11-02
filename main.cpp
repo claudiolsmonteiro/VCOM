4d95759fe56de49078cb75d7b6ec417b6742312f
claudiomonteiro <supahmomo@gmail.com>
2016-11-02 09:58:58 +0000

    ROI

--- main.cpp
+++ main.cpp
@@ -1,17 +1,56 @@
-// opencv_p01 - 2011/09/18 - JAS
-// Read and display an image
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <string>
 using namespace cv;
-int main()
+using namespace std;
+
+Mat image, regionOfInterest;
+vector<Point> roi;
+void CallBackFunction(int event, int x, int y, int flags, void* point)
 {
-	//read an image
-	Mat image = cv::imread("img.jpg");
-	//create image window named "My image"
-	namedWindow("My image"); //not necessary ... imshow() is enough...!
-							 //show the image on window
-	imshow("My image", image);
-	//wait key for 5000 ms
-	waitKey(5000); // waitKey(0); // waits for key press
+
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		Point p;
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+		p.x = x;
+		p.y = y;
+		roi.push_back(p);
+	}
+
+}
+
+int main(int argc, char** argv)
+{
+	Point p;
+	string imagename;
+	cout << "Please enter the name of the file: ";
+	getline(cin, imagename);
+	cout << "The value you entered is " << imagename;
+	image = imread(imagename, CV_LOAD_IMAGE_COLOR); // Read the file
+
+	while(!image.data) // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		cout << "Please enter the name of the file: ";
+		getline(cin, imagename);
+		cout << "The value you entered is " << imagename;
+		image = imread(imagename, CV_LOAD_IMAGE_COLOR); // Read the file
+	}
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
+	setMouseCallback("Display window", CallBackFunction, NULL);
+	imshow("Display window", image); // Show our image inside it.
+	while (cv::waitKey(1) != 27);
+	cout << roi[0].x << " " << roi[0].y << " " << roi[1].x << " " << roi[1].y;
+	regionOfInterest = image(Rect(roi[0].x, roi[0].y, roi[1].x - roi[0].x, roi[1].y - roi[0].y));
+	destroyWindow("Display Window");
+	namedWindow("Region of Interest", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Region of Interest", regionOfInterest); // Show our image inside it.
+
+	waitKey(0); // Wait for a keystroke in the window
+
 	return 0;
 }
